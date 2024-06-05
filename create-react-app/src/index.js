f28e39938c50083ce8a0922ebdbad2964cf5b53e
Ivan Semashko <itivansemashko@gmail.com>
2024-06-05 21:44:05 +0300

    Update: Create element two options

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
+// Better
 function Greeting() {
   return <h2>My First Component</h2>
+}
+
+// Bad
+function LongGreeting() {
+  return React.createElement('h2', {}, 'hello world')
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
