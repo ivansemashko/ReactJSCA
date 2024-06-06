faeffd3d362fe7ca5226e7092d46a99c2e0a90d0
Ivan Semashko <itivansemashko@gmail.com>
2024-06-06 17:08:47 +0300

    Update: Nest Components

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// Better
 function Greeting() {
   return (
-    <React.Fragment>
-      <div className="someValue">
-        <h3>Hello people</h3>
-        <ul>
-          <li>
-            <a href="#">Hello world</a>
-          </li>
-        </ul>
-      </div>
-      <h2>hello world</h2>
-      <input type="text" />
-    </React.Fragment>
+    <div>
+      <Person />
+      <Message />
+    </div>
   )
 }
 
-// Bad
-function LongGreeting() {
-  return React.createElement('h2', {}, 'hello world')
+const Person = () => <h2>Ivan Semashko</h2>
+const Message = () => {
+  return <p>This is my message</p>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
