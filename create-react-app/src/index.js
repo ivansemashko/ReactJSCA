1b4d424200a3caef2369fe4bff903d46d160ab42
Ivan Semashko <itivansemashko@gmail.com>
2024-06-06 23:59:44 +0300

    Update: Booklist

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,20 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Greeting() {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   )
 }
 
-const Person = () => <h2>Ivan Semashko</h2>
-const Message = () => {
-  return <p>This is my message</p>
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
 }
+
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/51ZvZFJOsrL._SL1499_.jpg"
+    alt=""
+  />
+)
+const Title = () => <h2>Book Title</h2>
+const Author = () => <h4>Author</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<BookList />)
