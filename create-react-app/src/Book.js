5d7cfba5d12c53042b943e790bb780863c7af119
Ivan Semashko <itivansemashko@gmail.com>
2024-06-16 17:20:24 +0300

    Update: Number Challenge

--- create-react-app/src/Book.js
+++ create-react-app/src/Book.js
@@ -1,11 +1,11 @@
 const Book = (props) => {
-  const { author, title, img } = props
-
+  const { author, title, img, number } = props
   return (
     <article className="book">
       <img src={img} alt={title + ' image'} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <span className="number">{`#${number + 1}`}</span>
     </article>
   )
 }
--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -8,8 +8,8 @@ import Book from './Book'
 const BookList = () => {
   return (
     <section className="bookList">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />
       })}
     </section>
   )
