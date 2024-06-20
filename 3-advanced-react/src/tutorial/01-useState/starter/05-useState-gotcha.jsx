745a6ea8c265a29d6bb94bb0b0256698df4c06a0
Ivan Semashko <itivansemashko@gmail.com>
2024-06-20 22:39:41 +0300

    Update: useState setTimeout()

--- 3-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 3-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -2,19 +2,19 @@ import { useState } from 'react'
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0)
 
+  const handleClick = () => {
+    setTimeout(() => {
+      setValue((currentState) => {
+        const newState = currentState + 1
+        return newState
+      })
+    }, 3000)
+  }
+
   return (
     <div>
       <h1>{value}</h1>
-      <button
-        className="btn"
-        type="button"
-        onClick={() => {
-          setValue((currentState) => {
-            const newState = currentState + 1
-            return newState
-          })
-        }}
-      >
+      <button className="btn" type="button" onClick={handleClick}>
         Click me
       </button>
     </div>
