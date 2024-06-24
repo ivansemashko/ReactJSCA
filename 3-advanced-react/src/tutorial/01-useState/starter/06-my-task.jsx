e3769837ba6c47434c44dc6db5660ea5b0ce3a10
Ivan Semashko <itivansemashko@gmail.com>
2024-06-24 09:59:06 +0300

    Update: My Example Button

--- 3-advanced-react/src/tutorial/01-useState/starter/06-my-task.jsx
+++ 3-advanced-react/src/tutorial/01-useState/starter/06-my-task.jsx
@@ -4,8 +4,11 @@ const windowOuterWidth = window.outerWidth
 const windowOuterHeight = window.outerHeight
 
 const bubbleButton = () => {
-  const [buttonWidth, setButtonWidth] = useState(windowOuterWidth / 2)
-  const [buttonHeight, setButtonHeight] = useState(windowOuterHeight / 2)
+  const [buttonWidth, setButtonWidth] = useState('50%')
+  const [buttonHeight, setButtonHeight] = useState('50%')
+  const [count, setCount] = useState(0)
+  const [buttonSizeWidth, setButtonSizeWidth] = useState('3%')
+  const [buttonSizeHeight, setButtonSizeHeight] = useState('3%')
 
   const randomCord = (maxCordHeight, maxCordWidth) => {
     let randomWidth = Math.floor(Math.random() * maxCordWidth)
@@ -13,26 +16,41 @@ const bubbleButton = () => {
     setButtonHeight(randomHeight)
     setButtonWidth(randomWidth)
   }
+  const randomButSize = () => {
+    let randomButWidth = Math.floor(Math.random() * 15)
+    let randomButHeight = Math.floor(Math.random() * 5)
+
+    setButtonSizeHeight(`${randomButHeight}%`)
+    setButtonSizeWidth(`${randomButWidth}%`)
+  }
+
+  const clickCount = () => {
+    setCount(count + 1)
+  }
 
   return (
-    <div>
-      <button
-        onMouseEnter={() => randomCord(windowOuterHeight, windowOuterWidth)}
-        onClick={() => alert('So nice!!')}
-        className="btn"
-        style={{
-          position: 'absolute',
-          left: buttonWidth,
-          top: buttonHeight,
-          width: '3%',
-          fontSize: '100%',
-          height: '3%',
-        }}
-        type="button"
-      >
-        Click
-      </button>
-    </div>
+    <>
+      <div>
+        <h2 style={{ pointerEvents: 'none' }}>{count}</h2>
+        <button
+          onMouseEnter={() => randomCord(windowOuterHeight, windowOuterWidth)}
+          onMouseLeave={() => randomButSize()}
+          onClick={() => clickCount()}
+          className="btn"
+          style={{
+            position: 'absolute',
+            left: buttonWidth,
+            top: buttonHeight,
+            width: buttonSizeWidth,
+            fontSize: '100%',
+            height: buttonSizeHeight,
+          }}
+          type="button"
+        >
+          Click
+        </button>
+      </div>
+    </>
   )
 }
 
