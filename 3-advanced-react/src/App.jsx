64a1734af6680d67ea3e4d1270bec3e490b6cd3e
Ivan Semashko <itivansemashko@gmail.com>
2024-06-30 17:28:14 +0300

    Update: Export Group

--- 3-advanced-react/src/App.jsx
+++ 3-advanced-react/src/App.jsx
@@ -1,15 +1,17 @@
+import { Test } from './Test'
 // import Home from './tutorial/04-project-structure/starter/Pages/Home'
 // import About from './tutorial/04-project-structure/starter/Pages/About'
-import {
-  Home,
-  About,
-} from './tutorial/04-project-structure/starter/Pages/index'
+import Example from './tutorial/04-project-structure/starter/Example'
+// import {
+//   Home,
+//   About,
+// } from './tutorial/04-project-structure/starter/Pages/index'
 
 function App() {
   return (
     <div className="container">
-      <Home />
-      <About />
+      <Example />
+      <Test />
     </div>
   )
 }
---
+++ 3-advanced-react/src/tutorial/04-project-structure/starter/Example/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstComponent = () => {
+  return (
+    <div>
+      <h2>First Component</h2>
+    </div>
+  )
+}
+
+export default FirstComponent
