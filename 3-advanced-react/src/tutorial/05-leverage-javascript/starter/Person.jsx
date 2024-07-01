1eb7abf2120e62b1ef4c8160e8379e504ca44c21
Ivan Semashko <itivansemashko@gmail.com>
2024-07-01 23:37:57 +0300

    Lvl Fix Js

--- 3-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ 3-advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-export function Person({ name, nickName, images }) {
+export function Person({ name, nickName = 'W/o', images }) {
   const img =
-    images?.[0]?.small?.url || 'https://www.computerhope.com/jargon/h/img.png'
+    images?.[0]?.small?.url ??
+    'https://avatars.githubusercontent.com/u/147409780?v=4'
   return (
     <div>
       <img src={img} style={{ width: '50px' }} alt={name} />
-      <h4>Name: {name}</h4>
+      <h4>
+        {img != 'https://avatars.githubusercontent.com/u/147409780?v=4'
+          ? 'Name: '
+          : 'Ivan Clone name: '}
+        {name}
+      </h4>
       <p>nickName: {nickName}</p>
     </div>
   )
