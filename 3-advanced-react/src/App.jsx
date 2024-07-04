5273550fe92501432edc06e9c4e1864608ac3f4d
Ivan Semashko <itivansemashko@gmail.com>
2024-07-04 19:17:37 +0300

    Update: FormData API

--- 3-advanced-react/src/App.jsx
+++ 3-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Started from './tutorial/06-forms/starter/04-other-inputs'
+import Started from './tutorial/06-forms/starter/05-form-data'
 function App() {
   return (
     <div className="container">
--- 3-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ 3-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -3,14 +3,6 @@ const frameworks = ['react', 'angular', 'vue', 'svelte']
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false)
   const [framework, setFramework] = useState('react')
-
-  const handleShipping = (e) => {
-    console.log(e.target.checked)
-    setShipping(e.target.checked)
-  }
-  const handleFramework = (e) => {
-    setFramework(e.target.value)
-  }
   return (
     <div>
       <form className="form">
