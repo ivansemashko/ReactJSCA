5ad7db79ac01a9b8079ce828e10997009ed5b025
Ivan Semashko <itivansemashko@gmail.com>
2024-07-06 20:39:56 +0300

    Update: 02-fetch-data.jsx (Fetch Custom Hook)

--- 3-advanced-react/src/App.jsx
+++ 3-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Started from './tutorial/08-custom-hooks/starter/01-toggle'
+import Started from './tutorial/08-custom-hooks/starter/02-fetch-data'
 function App() {
   return (
     <div className="container">
