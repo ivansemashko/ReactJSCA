f41a1ee4eda72a0d6e3941f4d0c83c1fcf037364
Ivan Semashko <itivansemashko@gmail.com>
2024-07-10 17:15:56 +0300

    Update: Context API setup

--- 3-advanced-react/src/tutorial/09-context-api/starter/Navbar.jsx
+++ 3-advanced-react/src/tutorial/09-context-api/starter/Navbar.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
 import NavLinks from './NavLinks'
 
+export const NavbarContext = React.createContext()
+
 const Navbar = () => {
   const [user, setUser] = useState({ name: 'bob' })
 
   const logout = () => setUser(null)
 
   return (
-    <nav className="navbar">
-      <h5>CONTEXT API</h5>
-      <NavLinks user={user} logout={logout} />
-    </nav>
+    <NavbarContext.Provider value={{ user, logout }}>
+      <nav className="navbar">
+        <h5>CONTEXT API</h5>
+        <NavLinks user={user} logout={logout} />
+      </nav>
+    </NavbarContext.Provider>
   )
 }
 
